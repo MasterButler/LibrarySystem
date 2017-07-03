226fafed13da89bb911d788335a219fc9159cc6f
Viteszeg <rofi_santos@dlsu.edu.ph>
2017-07-03 10:02:55 +0800

    Add Connection to Database

--- src/main/java/db/DBConnection.java
+++ src/main/java/db/DBConnection.java
@@ -1,5 +1,29 @@
 package db;
+import java.sql.*;
 
 public class DBConnection {
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/library_system";
+
+    //  Database credentials
+    static final String USER = "root";
+    static final String PASS = "root";
+
+    public Connection connect() {
+        Connection con;
+        String driver = "com.mysql.jdbc.Driver";
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(DB_URL, USER, PASS);
+            if (con == null) {
+                System.out.println("Connection cannot be established");
+            }
+            return con;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }
 
 }
